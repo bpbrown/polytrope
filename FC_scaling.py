326757e4e31d2267d33938ca2852600a111f9380
Benjamin Brown <bpbrown@astro.wisc.edu>
2015-01-23 18:50:15 -0700

    Building scaling scripts (based off examples-bpbrown/scaling), with modern use of docopt etc.

---
+++ FC_scaling.py
@@ -0,0 +1,168 @@
+import numpy as np
+import time
+import os
+import sys
+import equations
+
+# acquire scaling run parameters
+nx = np.int(os.environ['N_X'])
+nz = np.int(os.environ['N_Z'])
+
+import logging
+logger = logging.getLogger(__name__)
+
+from dedalus2.public import *
+from dedalus2.tools  import post
+from dedalus2.extras import flow_tools
+
+initial_time = time.time()
+
+logger.info("Starting Dedalus script {:s}".format(sys.argv[0]))
+
+# save data in directory named after script
+data_dir = sys.argv[0].split('.py')[0]+'/'
+
+Rayleigh = 4e4
+Prandtl = 1
+
+# Set domain
+Lz = 100
+Lx = 3*Lz
+    
+atmosphere = equations.FC_polytrope(nx=nx, nz=nz, Lx=Lx, Lz=Lz)
+atmosphere.set_IVP_problem(Rayleigh, Prandtl)
+atmosphere.set_BC()
+problem = atmosphere.get_problem()
+
+if atmosphere.domain.distributor.rank == 0:
+    if not os.path.exists('{:s}/'.format(data_dir)):
+        os.mkdir('{:s}/'.format(data_dir))
+
+ts = timesteppers.RK443
+cfl_safety_factor = 0.2*4
+
+
+# Build solver
+solver = problem.build_solver(ts)
+
+x = atmosphere.domain.grid(0)
+z = atmosphere.domain.grid(1)
+
+# initial conditions
+u = solver.state['u']
+w = solver.state['w']
+T = solver.state['T1']
+s = solver.state['s']
+ln_rho = solver.state['ln_rho1']
+
+solver.evaluator.vars['Lx'] = Lx
+solver.evaluator.vars['Lz'] = Lz
+
+for key in solver.problem.ncc_manager.ncc_strings:
+    logger.info("NCC: {}".format(key))
+
+A0 = 1e-6
+np.random.seed(1+atmosphere.domain.distributor.rank)
+
+T.set_scales(atmosphere.domain.dealias, keep_data=True)
+z_dealias = atmosphere.domain.grid(axis=1, scales=atmosphere.domain.dealias)
+T['g'] = A0*np.sin(np.pi*z_dealias/Lz)*np.random.randn(*T['g'].shape)*atmosphere.T0['g']
+
+logger.info("A0 = {:g}".format(A0))
+logger.info("T = {:g} -- {:g}".format(np.min(T['g']), np.max(T['g'])))
+
+logger.info("thermal_time = {:g}, top_thermal_time = {:g}".format(atmosphere.thermal_time, atmosphere.top_thermal_time))
+
+
+max_dt = atmosphere.buoyancy_time
+
+report_cadence = 1
+output_time_cadence = 0.1*atmosphere.buoyancy_time
+solver.stop_sim_time = 0.05*atmosphere.thermal_time
+solver.stop_iteration= np.inf
+solver.stop_wall_time = 0.25*3600
+
+logger.info("output cadence = {:g}".format(output_time_cadence))
+
+analysis_slice = solver.evaluator.add_file_handler(data_dir+"slices", sim_dt=output_time_cadence, max_writes=20, parallel=False)
+
+analysis_slice.add_task("s", name="s")
+analysis_slice.add_task("s - integ(s, 'x')/Lx", name="s'")
+analysis_slice.add_task("u", name="u")
+analysis_slice.add_task("w", name="w")
+analysis_slice.add_task("(dx(w) - dz(u))**2", name="enstrophy")
+
+
+do_checkpointing=False
+if do_checkpointing:
+    checkpoint = Checkpoint(data_dir)
+    checkpoint.set_checkpoint(solver, wall_dt=1800)
+
+
+    
+cfl_cadence = 1
+CFL = flow_tools.CFL(solver, initial_dt=max_dt, cadence=cfl_cadence, safety=cfl_safety_factor,
+                     max_change=1.5, min_change=0.5, max_dt=max_dt)
+
+CFL.add_velocities(('u', 'w'))
+
+# Flow properties
+flow = flow_tools.GlobalFlowProperty(solver, cadence=1)
+flow.add_property("sqrt(u*u + w*w)*Lz/ nu", name='Re')
+
+
+start_time = time.time()
+while solver.ok:
+
+    dt = CFL.compute_dt()
+    # advance
+    solver.step(dt)
+        
+    # update lists
+    if solver.iteration % report_cadence == 0:
+        log_string = 'Iteration: {:5d}, Time: {:8.3e}, dt: {:8.3e}, Re: {:8.3e}/{:8.3e}'.format(solver.iteration, solver.sim_time, dt,
+                                                                                                flow.grid_average('Re'), flow.max('Re'))
+        logger.info(log_string)
+        
+end_time = time.time()
+
+# Print statistics
+elapsed_time = end_time - start_time
+elapsed_sim_time = solver.sim_time
+N_iterations = solver.iteration 
+logger.info('main loop time: {:e}'.format(elapsed_time))
+logger.info('Iterations: {:d}'.format(N_iterations))
+logger.info('iter/sec: {:g}'.format(N_iterations/(elapsed_time)))
+logger.info('Average timestep: {:e}'.format(elapsed_sim_time / N_iterations))
+
+logger.info('beginning join operation')
+if do_checkpointing:
+    logger.info(data_dir+'/checkpoint/')
+    post.merge_analysis(data_dir+'/checkpoint/')
+logger.info(analysis_slice.base_path)
+post.merge_analysis(analysis_slice.base_path)
+
+if (atmosphere.domain.distributor.rank==0):
+
+    N_TOTAL_CPU = domain.distributor.comm_world.size
+    
+    # Print statistics
+    print('-' * 40)
+    total_time = end_time-initial_time
+    main_loop_time = end_time - start_time
+    startup_time = start_time-initial_time
+    print('  startup time:', startup_time)
+    print('main loop time:', main_loop_time)
+    print('    total time:', total_time)
+    print('Iterations:', solver.iteration)
+    print('Average timestep:', solver.sim_time / solver.iteration)
+    print('scaling:',
+          ' {:d} {:d} {:d}'.format(N_TOTAL_CPU,nx,nz),
+          ' {:8.3g} {:8.3g} {:8.3g} {:8.3g} {:8.3g}'.format(startup_time,
+                                                            main_loop_time, 
+                                                            main_loop_time/solver.iteration, 
+                                                            main_loop_time/solver.iteration/(nx*nz), 
+                                                            N_TOTAL_CPU*main_loop_time/solver.iteration/(nx*nz)))
+    print('-' * 40)
+
+
---
+++ scaling.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+"""
+Perform scaling runs on special scaling scripts.
+
+Usage:
+    scaling.py run <scaling_script>
+    scaling.py plot <files>... [--output=<dir>]
+
+Options:
+    --output=<dir>  Output directory [default: ./frames]
+
+"""
+import os
+import numpy as np
+import itertools
+import subprocess
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import time
+import shelve
+import scaling_plot
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+
+def do_scaling_run(scaling_script, resolution, CPU_set, test_type='minimal', mpirun='mpirun'):
+
+    print('testing {}, from {:d} to {:d} cores'.format(scaling_script, np.min(CPU_set),np.max(CPU_set)))
+    start_time = time.time()
+    
+    sim_nx = resolution[0] 
+    sim_nz = resolution[1]
+    
+    N_total_cpu = []
+    N_x = []
+    N_z = []
+    startup_time = []
+    wall_time = []
+    wall_time_per_iter = []
+    work = []
+    work_per_core = []
+    
+    for ENV_N_TOTAL_CPU in scaling_test_set:
+
+        print("scaling test of {}".format(scaling_script),
+              " at {:d}x{:d}".format(sim_nx, sim_nz),
+              " on {:d} cores".format(ENV_N_TOTAL_CPU))
+
+        test_env = dict(os.environ, 
+                        N_X='{:d}'.format(sim_nx),
+                        N_Z='{:d}'.format(sim_nz),
+                        N_TOTAL_CPU='{:d}'.format(ENV_N_TOTAL_CPU))
+
+        proc = subprocess.Popen([mpirun, "-np","{:d}".format(ENV_N_TOTAL_CPU), "python3", scaling_script], 
+                                env=test_env,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        stdout, stderr = proc.communicate()
+
+        for line in stdout.splitlines():
+            if line.startswith('scaling:'):
+                split_line = line.split()
+                print(split_line)
+                N_total_cpu.append(num(split_line[1]))
+
+                N_x.append(num(split_line[2]))
+                N_z.append(num(split_line[3]))
+
+                startup_time.append(num(split_line[4]))
+                wall_time.append(num(split_line[5]))
+                wall_time_per_iter.append(num(split_line[6]))
+
+                work.append(num(split_line[7]))
+                work_per_core.append(num(split_line[8]))
+
+    # change data storage to numpy arrays
+    N_total_cpu = np.array(N_total_cpu)
+    N_x = np.array(N_x)
+    N_z = np.array(N_z)
+    startup_time = np.array(startup_time)
+    wall_time = np.array(wall_time)
+    wall_time_per_iter = np.array(wall_time_per_iter)
+    work = np.array(work)
+    work_per_core = np.array(work_per_core)
+    
+    print(40*'-')
+    print("scaling results")
+    for i, temp in enumerate(N_total_cpu):
+        print(N_total_cpu[i], N_z[i], startup_time[i], wall_time[i], wall_time_per_iter[i])
+
+    data_set = [sim_nx, 0, sim_nz, 
+                N_total_cpu, 0, N_total_cpu,
+                N_x, N_x, N_z, 
+                startup_time, wall_time, wall_time_per_iter,
+                work, work_per_core] # hacked
+
+    write_scaling_run(data_set)
+
+    end_time = time.time()
+    print(40*'*')
+    print('time to test {:d}x{:d}: {:8.3g}'.format(sim_nx,sim_nz, end_time-start_time))
+    print(40*'*')
+
+    return data_set
+
+def write_scaling_run(data_set):
+    sim_nx = data_set[0]
+    sim_ny = data_set[1]
+    sim_nz = data_set[2]
+    N_x_cpu = data_set[3]
+    N_y_cpu = data_set[4]
+    N_total_cpu = data_set[5]
+    N_x = data_set[6]
+    N_y = data_set[7]
+    N_z = data_set[8]
+    startup_time = data_set[9]
+    wall_time = data_set[10]
+    wall_time_per_iter = data_set[11]
+    work = data_set[12]
+    work_per_core = data_set[13]
+        
+    resolution_string = '{:d}x{:d}x{:d}'.format(sim_nx, sim_ny, sim_nz)
+    scaling_file = shelve.open('scaling_data_'+resolution_string+'.db', flag='n')
+    scaling_file['nx'] = sim_nx
+    scaling_file['ny'] = sim_ny
+    scaling_file['nz'] = sim_nz
+    scaling_file['N_x_cpu'] = N_x_cpu
+    scaling_file['N_y_cpu'] = N_y_cpu
+    scaling_file['N_total_cpu'] = N_total_cpu
+    scaling_file['N_x'] = N_x
+    scaling_file['N_y'] = N_y
+    scaling_file['N_z'] = N_z
+    scaling_file['startup_time'] = startup_time
+    scaling_file['wall_time'] = wall_time
+    scaling_file['wall_time_per_iter'] = wall_time_per_iter
+    scaling_file['work'] = work
+    scaling_file['work_per_core'] = work_per_core
+    scaling_file.close()
+    
+
+    
+if __name__ == "__main__":
+    
+    from docopt import docopt
+
+    start_time = time.time()
+
+    args = docopt(__doc__)
+    if args['run']:
+        CPU_set = [1, 2] #4, 8, 16, 32, 64]
+        resolution = [192, 96]
+        data_set = do_scaling_run(args['<scaling_script>'], resolution, CPU_set, mpirun='mpirun')
+    elif args['plot']:
+        output_path = pathlib.Path(args['--output']).absolute()
+        # Create output directory if needed
+        with Sync() as sync:
+            if sync.comm.rank == 0:
+                if not output_path.exists():
+                    output_path.mkdir()
+        #data_set = use shelve to read in (args['<files>'])
+
+    fig_set, ax_set = scaling_plot.initialize_plots(4)
+    scaling_plot.plot_scaling_run(data_set, ax_set)
+    
+    scaling_plot.finalize_plots(fig_set, ax_set)
+
+    end_time = time.time()
+    print(40*'=')
+    print('time to do all tests: {:f}'.format(end_time-start_time))
+    print(40*'=')
+
