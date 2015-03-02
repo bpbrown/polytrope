23772115b406a732b10af664c9f5ae68e864768a
Benjamin Brown <bpbrown@astro.wisc.edu>
2015-03-02 15:48:49 -0700

    Updates to join_data (pulled from old_parser bookmark)

--- join_data.py
+++ join_data.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import logging
+logger = logging.getLogger(__name__)
 
-from dedalus.tools.logging import logger
+import dedalus.public
 from dedalus.tools  import post
 
 data_dir = sys.argv[1]
@@ -10,8 +12,10 @@ base_path = os.path.abspath(data_dir)+'/'
 logger.info("joining data from Dedalus run {:s}".format(data_dir))
 logger.info("merging checkpoint")
 post.merge_analysis(base_path+'checkpoint')
-logger.info("merging profile_data")
-post.merge_analysis(base_path+'profile_data')
+logger.info("merging scalar")
+post.merge_analysis(base_path+'scalar')
+logger.info("merging profile")
+post.merge_analysis(base_path+'profile')
 logger.info("merging slices")
 post.merge_analysis(base_path+'slices')
 logger.info("done join operation for {:s}".format(data_dir))
