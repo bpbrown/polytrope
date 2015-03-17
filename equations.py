d025e93c4c0c1c72031f5efe7ab6e2d4ec689f4c
Benjamin Brown <bpbrown@astro.wisc.edu>
2015-03-17 12:36:40 -0600

    Fixed subtle problem in equations.py; ncc phi, though not used in equations, needs to be field specified.  Otherwise ugly numpy error on first iter.

--- equations.py
+++ equations.py
@@ -71,7 +71,8 @@ class polytrope:
 
         self.g = self.poly_n + 1
         # choose a particular gauge for phi; and -grad(phi)=g
-        self.phi = self.g*(self.z0 - self.z)
+        self.phi = self._new_ncc()
+        self.phi['g'] = self.g*(self.z0 - self.z)
         
         logger.info("polytropic atmosphere parameters:")
         logger.info("   poly_n = {:g}, epsilon = {:g}, gamma = {:g}".format(self.poly_n, self.epsilon, self.gamma))
