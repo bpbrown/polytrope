4ef8e826cf2d5fc6e0a2f800cc8cf96fe3bcceed
Evan Anders <evan.anders@colorado.edu>
2015-07-29 13:05:55 -0600

    Adds logger to file merge function; removes two deprecated functions

--- FC_onset_solve.py
+++ FC_onset_solve.py
@@ -155,151 +155,6 @@ class FC_onset_solver:
 
         return (unstables, self._positive_eigenvalues_r)
     
-    def solve_unstable_modes_parallel(self, ra, **kwargs):
-        '''
-        Solves the EVP at all horizontal wavenumbers for the specified value of
-        ra and collects all unstable modes at that ra.
-        '''
-        kxs = int(nx/2)
-        kxs_global = np.arange(kxs)
-        kxs_local = kxs_global[CW.rank::CW.size]
-
-        returns_local = [self.get_unstable_modes(ra, kx, **kwargs) for kx in kxs_local]
-        if CW.rank == 0:
-            returns_global = [0]*kxs
-            returns_global[CW.rank::CW.size] = returns_local
-        
-        CW.Barrier()
-        for i in range(CW.size):
-            if i == 0:
-                continue
-            if i == CW.rank:
-                CW.send(returns_local, dest=0, tag=i)
-            elif CW.rank == 0:
-                vals = CW.recv(source=i, tag=i)
-                returns_global[i::CW.size] = vals
-        CW.Barrier()
-        if CW.rank == 0:
-            return returns_global
-        else:
-            return True
-
-    def plot_onsets(self, ra_range, **kwargs):
-        if CW.rank == 0:
-            import matplotlib.pyplot as plt
-        evals = dict()
-        prof_returns = dict()
-        if 'profiles' in kwargs.keys():
-            profiles = kwargs['profiles']
-        for ra in ra_range:
-            returned = self.solve_unstable_modes_parallel(ra, **kwargs)
-            if CW.rank == 0:
-                for i in range(len(returned)):
-                    item = returned[i]
-                    if np.asarray(item[0]).shape[0] > 0:
-                        eigval_array = np.asarray(item[1])
-                        eigvals = []
-                        for j in range(eigval_array.shape[1]):
-                            eigval = eigval_array[:,j][1]
-                            eigvals.append((ra, eigval))
-                        if str(i) not in evals.keys():
-                            evals[str(i)] = eigvals
-                        else:
-                            for pack in eigvals:
-                                evals[str(i)].append(pack)
-                        for j in range(np.asarray(item[0]).shape[0]):
-                            profiles_returned = np.asarray(item[0])[j,:]
-                            count = 0
-                            for key in profiles:
-                                new_key = "{0}_".format(i) + key
-                                if new_key not in prof_returns.keys():
-                                    prof_returns[new_key] = [profiles_returned[count,:]]
-                                else:
-                                    prof_returns[new_key].append(profiles_returned[count,:])
-                                count += 1
-                            
-        if CW.rank == 0:
-            print('saving output')
-            import h5py
-
-            outstring = self.out_dir+'evals_eps_{0:.0e}_ras_{1:04g}-{2:04g}_nrho_{3:.1f}'.format(self.epsilon, ra_range[0], ra_range[-1], self.n_rho_cz)
-            f = h5py.File(outstring +'.h5', 'w')
-            file_keys = []
-
-            plt.figure(figsize=(15,10))
-            for key in evals.keys():
-                ras = []
-                evals_local = []
-                for pack in evals[key]:
-                    ras.append(pack[0])
-                    evals_local.append(pack[1])
-                plt.plot(ras, evals_local, label='wavenum='+key)
-#                print('wavenum {0} has {1} at {2}'.format(int(key), ras, evals_local))
-                dict_key_ra = '{0}_ras'.format(int(key))
-                dict_key_eval = '{0}_evals'.format(int(key))
-                f[dict_key_ra] = ras
-                f[dict_key_eval] = evals_local
-                file_keys.append(dict_key_ra)
-                file_keys.append(dict_key_eval)
-                for p_key in profiles:
-                    my_key = "{0}_".format(int(key))+p_key
-                    f[my_key] = np.asarray(prof_returns[my_key])
-                    file_keys.append(my_key)
-
-            file_keys.append('x')
-            file_keys.append('z')
-            f['x'] = self.x
-            f['z'] = self.z
-            print(file_keys)
-                    
-
-            #Storing string list from http://stackoverflow.com/questions/23220513/storing-a-list-of-strings-to-a-hdf5-dataset-from-python
-            asciiList = [n.encode('ascii', 'ignore') for n in file_keys]
-            f.create_dataset('keys', (len(asciiList),1), 'S10', asciiList)
-            #plt.axhline(self.epsilon, linestyle='dashed', color='black', label=r'$\epsilon$')
-            plt.legend(loc='lower left')
-            plt.xlabel('Ra')
-            plt.ylabel(r'$Re(\omega)$')
-            plt.yscale('log')
-            plt.savefig(outstring + '.png', dpi=200)
-
-            
-    def find_onset_ra(self, start=1, end=4, tol=1e-2):
-        '''
-        Steps in log space (specified by start (10**start) and end (10**end))
-        to find the highest value of Ra at which there are no unstable modes,
-        returns the critical rayleigh number.  Tol determines the accuracy
-        of the solve.
-        '''
-        N = 5
-        first = True
-        while(end/start > 1+tol):
-            ras = np.logspace(start, end, N)
-            if not first:
-                ras = ras[1:]
-            else:
-                first = False
-            for i in range(N):
-                if CW.rank == 0:
-                    print('Solving at Ra {0}...'.format(ras[i]))
-                unstables = self.solve_unstable_modes_parallel(ras[i])
-                if CW.rank == 0:
-                    for j in range(len(unstables)):
-                        if unstables[j][1].shape[1] > 0:
-                            end = np.log10(ras[i])
-                            break
-                    if end != np.log10(ras[i]):
-                        start = np.log10(ras[i])
-                info = CW.bcast(obj=[start,end], root=0)
-                start = info[0]
-                end = info[1]
-                if end == np.log10(ras[i]):
-                    break
-        crit = (end+start)/2
-        if CW.rank == 0:
-            print('Critical Ra of {0:.4g} found'.format(10**crit))
-        return 10**crit
-
     def full_parallel_solve(self, stitch_files=True):
         kxs = int(nx/2)
         kxs_global = list(np.arange(kxs))
@@ -334,6 +189,7 @@ class FC_onset_solver:
             self.stitch_files()
 
     def stitch_files(self):
+        logger.info('Merging files...')
         filename = self.out_dir + self.out_file_name + '.h5'
         file_all = h5py.File(filename, 'w')
         keys = []
@@ -466,7 +322,7 @@ if __name__ == '__main__':
     n_rho_cz = [3.5, 10, 20]
 
     for n_rho in n_rho_cz:
-        solver = FC_onset_solver(np.logspace(1, 3, 2), profiles=['u','w','T1'],nx=nx, nz=nz, Lx=Lx, n_rho_cz=n_rho, epsilon=epsilon, comm=MPI.COMM_SELF, out_dir=out_dir, constant_kappa=True)
+        solver = FC_onset_solver(np.logspace(1, 3, 100), profiles=['u','w','T1'],nx=nx, nz=nz, Lx=Lx, n_rho_cz=n_rho, epsilon=epsilon, comm=MPI.COMM_SELF, out_dir=out_dir, constant_kappa=True)
         solver.full_parallel_solve()
         wavenumbers, profiles, filename, atmosphere = solver.read_file()
         solver.plot_growth_modes(wavenumbers, filename)
