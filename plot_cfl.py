222a49ef297f02334a44efa495e2f7b26a349bba
Benjamin Brown <bpbrown@astro.wisc.edu>
2015-10-27 13:57:07 -0600

    Updates to plot_cfl to include --fast calculations.

--- plot_cfl.py
+++ plot_cfl.py
@@ -8,6 +8,7 @@ Usage:
 Options:
     --output=<output>         Output directory; if blank a guess based on likely case name will be made
     --fields=<fields>         Comma separated list of fields to plot [default: u,w]
+    --fast                    Plot "fast" CFL version (using mean z-spacing rather than grid spacing)
 """
 import numpy as np
 import analysis
@@ -18,15 +19,19 @@ logger = logging.getLogger(__name__.split('.')[-1])
 import plot_slices
 
 def main(files, fields, output_path='./', output_name='cfl',
-         static_scale=True):
+         static_scale=False, fast=False):
     
     from mpi4py import MPI
 
     comm_world = MPI.COMM_WORLD
     rank = comm_world.rank
     size = comm_world.size
+
+    if fast:
+        output_name += '_fast'
     
     data = analysis.Slice(files)
+
     
     # select down to the data you wish to plot
     data_list = []
@@ -37,9 +42,13 @@ def main(files, fields, output_path='./', output_name='cfl',
     dx = np.diff(data.x)
     dx = np.append(dx, dx[-1])
     dx = dx.reshape(dx.shape[0],1)
-    dz = np.diff(data.z)
-    dz = np.append(dz, dz[-1])
-    dz = dz.reshape(1, dz.shape[0])
+    if fast:
+        # mean spacing over whole sim; not quite one-to-one with the CZ based spacing, but close.
+        dz = (data.z[-1]-data.z[0])/data.z.shape[0]
+    else:
+        dz = np.diff(data.z)
+        dz = np.append(dz, dz[-1])
+        dz = dz.reshape(1, dz.shape[0])
 
     frequencies = np.abs(data_list[0])/dx +  np.abs(data_list[1])/dz
     cfl = 1/frequencies
@@ -75,7 +84,10 @@ def main(files, fields, output_path='./', output_name='cfl',
         imagestack = plot_slices.ImageStack(data.x, data.z, current_data, freq_list, verbose=False, percent_cut=0)
         max_freq_loc = np.unravel_index(frequencies.argmax(), frequencies.shape)
 
-        logger.info("cfl limit of  at {} : ({}, {})".format(max_freq_loc, data.x[max_freq_loc[0]], data.z[max_freq_loc[1]]))
+        logger.info("cfl limit of {:8.3g}s or {:8.3g}/s at {} : ({:8.3g}, {:8.3g})".format(cfl[max_freq_loc],
+                                                                       frequencies[max_freq_loc],
+                                                                       max_freq_loc,
+                                                                       data.x[max_freq_loc[0]], data.z[max_freq_loc[1]]))
         for image in imagestack.images:
             # Mark where the max frequency occurs (CFL determining location)
             #image.imax.plot([data.x[max_freq_loc[0]], data.z[max_freq_loc[1]]], marker='0', color='green')
@@ -129,6 +141,6 @@ if __name__ == "__main__":
         post.visit_writes(args['<files>'],  accumulate_files, file_list=file_list)
             
         if len(file_list) > 0:
-            main(file_list, fields, output_path=str(output_path)+'/')
+            main(file_list, fields, output_path=str(output_path)+'/', fast=args['--fast'])
 
 
