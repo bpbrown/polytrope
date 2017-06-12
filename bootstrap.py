529ef86b3f2cbc34920028ad922e7e24cad18b4f
J.S. Oishi <jsoishi@gmail.com>
2017-06-12 13:50:55 -0700

    added more functionality to bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,18 +1,24 @@
+import os
+import re
 import logging
 import numpy as np
 from FC_multi import FC_convection
-def bootstrap(Ra_start, Ra_end, flags, nx=None, nz=None, nproc=None, run_function=FC_convection, step_run_time=50.):
-    """bootstrap run_function in steps of 10 from Ra_start to Ra_end.
+
+def bootstrap(init_file, Ra_end, flags, nx=None, nz=None, nproc=None, run_function=FC_convection, step_run_time=50.):
+    """bootstrap run_function in steps of 10 from Ra_start to Ra_end. The function assumes Ra_start has already been run.
 
     nx: if supplied, a list of length equal to the number of steps 
     nz: if supplied, a list of length equal to the number of steps 
     nproc: if supplied, a list of length equal to the number of steps 
-
+    run_function: the function that runs the Dedalus simulation
+    step_run_time: the amount of simulation time to run each step
     """
 
     logger = logging.getLogger(__name__)
 
+    Ra_start = float(re.search("Ra(\d+)",init_file).groups()[0])
     n_steps = int(np.log10(Ra_end/Ra_start))
+    assert n_steps > 0
     logger.info("Bootstrapping from Ra = {:5.2e} to Ra = {:5.2e} in {:d} steps".format(Ra_start, Ra_end, n_steps))
 
     if nx:
@@ -39,17 +45,20 @@ def bootstrap(Ra_start, Ra_end, flags, nx=None, nz=None, nproc=None, run_functio
     flags['run_time_buoyancies'] = step_run_time
     
     Ra = Ra_start
+    restart_file = init_file
     for i in range(n_steps-1):
         print("Step {}".format(i))
         print("================")
         Ra *= 10
         flags['Rayleigh'] = Ra
+        flags['restart'] = restart_file
         if nx[i]:
             flags['nx'] = nx[i]
         if nz[i]:
             flags['nz']  = nz[i]
-        run_function(**flags)
-
+        dir_name = run_function(**flags)
+        restart_file = os.path.join(dir_name,"final_checkpoint","final_checkpoint_s1.h5")
+        
     print("================")
     print("Beginning target run.")
     flags['Rayleigh'] = Ra_end
@@ -66,6 +75,10 @@ def test_function(**kwargs):
         print("{}: {}".format(k,v))
     print()
 
+    dir_name = "scratch/FC_multi_Ra{}_S{}".format(kwargs['Rayleigh'],kwargs['Stiffness'])
+    return dir_name
+
 if __name__ == "__main__":
     flags = {"Rayleigh": 1e3, "Stiffness":10}
-    bootstrap(1e3,1e8,flags,run_function=test_function,nx = [512,512,512,1024,1536])
+    start_file = "scratch/FC_multi_nrhocz3_Ra1000_S10"
+    bootstrap(start_file,1e8,flags,run_function=test_function,nx = [512,512,512,1024,1536])
