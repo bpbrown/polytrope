d8ac27fa174349393e9d96405b2d745eaf8d6dde
Benjamin Brown <bpbrown@colorado.edu>
2017-08-23 15:47:25 -0700

    Bug fix: removing chemistry flags that snuck in during merge.

--- stratified_dynamics/polytropes.py
+++ stratified_dynamics/polytropes.py
@@ -43,17 +43,17 @@ class FC_polytrope_2d_kappa_mu(FC_equations_2d_kappa_mu, Polytrope):
 
                      
 class FC_polytrope_3d(FC_equations_3d, Polytrope):
-    def __init__(self, dimensions=3, chemistry=False, *args, **kwargs):
-        super(FC_polytrope_3d, self).__init__(dimensions=dimensions,chemistry=chemistry) 
+    def __init__(self, dimensions=3, *args, **kwargs):
+        super(FC_polytrope_3d, self).__init__(dimensions=dimensions) 
         Polytrope.__init__(self, dimensions=dimensions, *args, **kwargs)
         logger.info("solving {} in a {} atmosphere".format(self.equation_set, self.atmosphere_name))
 
-    def set_equations(self, *args, chemistry=False, **kwargs):
-        super(FC_polytrope_3d, self).set_equations(*args, chemistry=chemistry,**kwargs)
+    def set_equations(self, *args, **kwargs):
+        super(FC_polytrope_3d, self).set_equations(*args, **kwargs)
         self.test_hydrostatic_balance(T=self.T0, rho=self.rho0)
         
     def initialize_output(self, solver, data_dir, *args, **kwargs):
-        super(FC_polytrope_3d, self).initialize_output(solver, data_dir, *args, chemistry=chemistry,**kwargs)
+        super(FC_polytrope_3d, self).initialize_output(solver, data_dir, *args, **kwargs)
         self.save_atmosphere_file(data_dir)
         return self.analysis_tasks
 
